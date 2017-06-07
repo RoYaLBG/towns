afc9ca2113c403ad92ee7a458f9c8934a15e1023
RoYaLBG <royal4o@gmail.com>
2017-06-07 15:44:57 +0300

    revert the pesho's idiotic revert

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Towns
 Exercise for students in the teamwork course at SoftUni.
+Edit by Gosho
+Edit by Pesho
--- towns.js
+++ towns.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown);
+	$('#btnAdd').click(() => addTown($("#townName").val()));
+	$('#btnShuffle').click(shuffleTowns);
 });
 
 function deleteTown() {
@@ -16,4 +18,28 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+
+function addTown(town) {
+	$("#towns").append("<option>" + town + "</option>");
+    $('#result').text(town + " added succesfully.");
+}
+
+function shuffleTowns() {
+	let children = $('#towns').children();
+	_shuffle(children);
+	$('#towns').html();
+	for (let i = 0; i < children.length; i++) {
+        $('#towns').append(children[i]);
+	}
+}
+
+function _shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
 }
